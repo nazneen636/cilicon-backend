254afbaeb3b1798338e22dbca209b6ad13e2b952
nazneen636 <nazneennahar636@gmail.com>
2025-08-20 00:05:18 +0600

    multer add

--- src/models/category.model.js
+++ src/models/category.model.js
@@ -1,6 +1,7 @@
 const { required, boolean } = require("joi");
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const slugify = require("slugify");
 
 const categorySchema = new Schema(
   {
@@ -16,10 +17,6 @@ const categorySchema = new Schema(
     },
     slug: {
       type: String,
-      trim: true,
-      require: true,
-      unique: true,
-      lowercase: true,
     },
     subCategory: [],
     discount: [],
@@ -30,6 +27,29 @@ const categorySchema = new Schema(
   },
   { Timestamp: true }
 );
+
+// make a slugify
+categorySchema.pre("save", async function (next) {
+  if (this.isModified("name")) {
+    this.slug = slugify(this.name, {
+      replacement: "-",
+      remove: undefined,
+      lower: true,
+      locale: "vi",
+      trim: true,
+    });
+  }
+  next();
+});
+
+// check user email and phone already exists or not
+categorySchema.pre("save", async function (next) {
+  const isExists = await this.constructor.findOne({ slug: this.slug });
+  if (isExists && isExists._id.toString() !== this.id.toString()) {
+    throw new Error(401, `${this.name} category already exists, try another`);
+  }
+  next();
+});
 
 module.exports =
   mongoose.model("Category", categorySchema) || mongoose.models.Category;
